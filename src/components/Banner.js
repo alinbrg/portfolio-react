199b6cbaf9cd168f07475dcb37127bf215802f6b
alinbrg <alina.bregvadze@gmail.com>
2022-09-07 17:59:51 +0400

    hero section

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,1 +1,70 @@
-export const Banner = () => {};
+import { Col, Container, Row } from "react-bootstrap";
+import { ArrowRightCircle } from "react-bootstrap-icons";
+import headerImg from "../assets/img/header-img.svg";
+
+import { useState, useEffect } from "react";
+
+export const Banner = () => {
+	const [isDeleting, setIsDeleting] = useState(false);
+	const toRotate = ["Front-End Developer"];
+	const [text, setText] = useState("");
+	const [delta, setDelta] = useState(300 - Math.random() * 100);
+	const period = 2000;
+
+	useEffect(() => {
+		let ticker = setInterval(() => {
+			tick();
+		}, delta);
+		return () => {
+			clearInterval(ticker);
+		};
+	}, [text]);
+
+	const tick = () => {
+		let fullText = toRotate[0];
+		// console.log(fullText);
+		let updateText = isDeleting
+			? fullText.substring(0, text.length - 1)
+			: fullText.substring(0, text.length + 1);
+
+		setText(updateText);
+
+		if (isDeleting) {
+			setDelta((prevDelta) => prevDelta / 2);
+		}
+
+		if (!isDeleting && updateText === fullText) {
+			setIsDeleting(true);
+			setDelta(period);
+		} else if (isDeleting && updateText === "") {
+			setIsDeleting(false);
+			setDelta(500);
+		}
+	};
+
+	return (
+		<section className="banner" id="home">
+			<Container>
+				<Row className="align-items-center">
+					<Col xs={12} md={6} xl={7}>
+						<span className="tagline">Welcome to my portfolio</span>
+						<h1>
+							{`Hi I'm Alina`}
+							<span className="wrap">, {text}</span>
+						</h1>
+						<p>
+							Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolor,
+							delectus! Nisi, eveniet a non magnam cum, libero cumque,
+						</p>
+						<button onClick={() => console.log("connect")}>
+							Let's connect <ArrowRightCircle size={25} />
+						</button>
+					</Col>
+					<Col xs={12} md={6} xl={5}>
+						<img src={headerImg} alt="Header Image" />
+					</Col>
+				</Row>
+			</Container>
+		</section>
+	);
+};
